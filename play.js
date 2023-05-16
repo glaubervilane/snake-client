80ea5ea061a78475c0295509ad990b933f168cb1
Glauber Marques <glauberfmarques@gmail.com>
2023-05-16 06:37:07 -0600

    Implement a user input event handler to manage snake moves on next steps

--- play.js
+++ play.js
@@ -1,4 +1,29 @@
 const { connect } = require('./client');
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", (data) => {
+    handleUserInput(data, conn);
+  });
+  return stdin;
+};
+
+const handleUserInput = function (data, conn) {
+  if (data === '\u0003') {
+    process.exit(); // Terminate the game if Ctrl + C is pressed
+  } else {
+    // Handle other key inputs here
+    // For example, send "Move: up" command for the 'w' key
+    if (data === 'w') {
+      conn.write("Move: up");
+    }
+    // Add more key handlers for other movements here
+  }
+};
+
 console.log("Connecting ...");
-connect();
+const conn = connect();
+setupInput(conn);
