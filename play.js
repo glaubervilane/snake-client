2071b07f16749b5d4a6db1d4498e59b643991879
Glauber Marques <glauberfmarques@gmail.com>
2023-05-15 20:33:18 -0600

    Updating client.js to get the cirrect data from the connection

--- play.js
+++ play.js
@@ -8,7 +8,12 @@ const connect = function () {
   });
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
+  conn.on("connect", () => { 
+    console.log("connect");
+  })
+  conn.on("data", (data) => { 
+    console.log(data);
+  })
   return conn;
 };
 
