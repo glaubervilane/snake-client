dcc08dfcd4fcd9a9663b78574b2058317734c69e
Glauber Marques <glauberfmarques@gmail.com>
2023-05-16 06:06:46 -0600

    Adding a new event handler to output initials on server screen

--- client.js
+++ client.js
@@ -14,6 +14,10 @@ const connect = function () {
   conn.on("data", (data) => { 
     console.log(data);
   })
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: GFM");
+  })
   return conn;
 };
 
