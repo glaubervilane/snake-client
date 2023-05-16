d6c4a182895a381596435f3290ff39cf77e73cf0
Glauber Marques <glauberfmarques@gmail.com>
2023-05-16 05:52:58 -0600

    Upadating play.js using imported connection from client file

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host:  '172.22.25.40',// IP address here,
+    port: 50541,// PORT number here,
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", () => { 
+    console.log("connect");
+  })
+  conn.on("data", (data) => { 
+    console.log(data);
+  })
+  return conn;
+};
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,21 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host:  '172.22.25.40',// IP address here,
-    port: 50541,// PORT number here,
-  });
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => { 
-    console.log("connect");
-  })
-  conn.on("data", (data) => { 
-    console.log(data);
-  })
-  return conn;
-};
+const { connect } = require('./client');
 
 console.log("Connecting ...");
 connect();
