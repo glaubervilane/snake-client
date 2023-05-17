a185633880a116b99fafe2a2ccb1579c996a7b2f
Glauber Marques <glauberfmarques@gmail.com>
2023-05-17 05:51:27 -0600

    Getting static constants to other file to separate from the main code

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host:  '172.22.25.40',// IP address here,
-    port: 50541,// PORT number here,
+    host: IP,// IP address here,
+    port: PORT,// PORT number here,
   });
   // interpret incoming data as text
   conn.setEncoding("utf8");
@@ -17,23 +18,7 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: GFM");
-    // sendMoves();
   })
-
-  // function sendMoves() {
-  //   setTimeout(() => {
-  //     conn.write("Move: up");
-  //   }, 50);
-  //   setTimeout(() => {
-  //     conn.write("Move: down");
-  //   }, 100);
-  //   setTimeout(() => {
-  //     conn.write("Move: right");
-  //   }, 150);
-  //   setTimeout(() => {
-  //     conn.write("Move: left");
-  //   }, 200);
-  // }
   return conn;
 };
 
---
+++ constants.js
@@ -0,0 +1,7 @@
+const IP = '172.22.25.40';
+const PORT = 50541;
+
+module.exports = {
+  IP,
+  PORT,
+};
