e7369bbb3ac29498f1d3180e82154b719b017a11
Glauber Marques <glauberfmarques@gmail.com>
2023-05-18 06:47:21 -0600

    Update input file implementing new changes to snke game commands and behavior

--- input.js
+++ input.js
@@ -1,16 +1,28 @@
 const { keyMappings } = require("./constants");
 let connection;
+let currentDirection;
 
 const setupInput = function(conn) {
   connection = conn;
+  currentDirection = "right"; // Initial direction
 
   const handleUserInput = function(key) {
     if (key === '\u0003') {
       process.exit();
     } else if (keyMappings[key]) {
-      connection.write(keyMappings[key]);
+      const command = keyMappings[key];
+      if (command.startsWith("Move:")) {
+        const direction = command.split(":")[1].trim();
+        currentDirection = direction;
+      }
+      connection.write(command);
     }
-  }
+  };
+
+  const sendContinuousMovement = function() {
+    const movementCommand = `Move: ${currentDirection}`;
+    connection.write(movementCommand);
+  };
 
   const stdin = process.stdin;
   stdin.setRawMode(true);
@@ -18,6 +30,9 @@ const setupInput = function(conn) {
   stdin.resume();
 
   stdin.on('data', handleUserInput);
+
+  // Send movement commands every 100ms
+  setInterval(sendContinuousMovement, 100);
 
   return stdin;
 };
